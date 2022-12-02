748a2cf26446bbb77fe951f1f7ec2471eab5dc26
monkey codes <jz@monkey.codes>
2022-12-02 14:43:08 +1000

    Debug service

--- bootstrap-pi/github-webhook-receiver-service/listener.py
+++ bootstrap-pi/github-webhook-receiver-service/listener.py
@@ -15,7 +15,6 @@ import json
 # since it is subscribed to that same topic.
 
 import command_line_utils;
-print("Script invoked {}".format(__name__))
 # Parse arguments
 cmdUtils = command_line_utils.CommandLineUtils("PubSub - Send and recieve messages through an MQTT connection.")
 cmdUtils.add_common_mqtt_commands()
@@ -35,41 +34,42 @@ received_count = 0
 received_all_event = threading.Event()
 is_ci = cmdUtils.get_command("is_ci", None) != None
 
-## Callback when connection is accidentally lost.
-#def on_connection_interrupted(connection, error, **kwargs):
-#    print("Connection interrupted. error: {}".format(error))
-#
-#
-## Callback when an interrupted connection is re-established.
-#def on_connection_resumed(connection, return_code, session_present, **kwargs):
-#    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
-#
-#    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
-#        print("Session did not persist. Resubscribing to existing topics...")
-#        resubscribe_future, _ = connection.resubscribe_existing_topics()
-#
-#        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
-#        # evaluate result with a callback instead.
-#        resubscribe_future.add_done_callback(on_resubscribe_complete)
-#
-#
-#def on_resubscribe_complete(resubscribe_future):
-#        resubscribe_results = resubscribe_future.result()
-#        print("Resubscribe results: {}".format(resubscribe_results))
-#
-#        for topic, qos in resubscribe_results['topics']:
-#            if qos is None:
-#                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
-#
-#
-## Callback when the subscribed topic receives a message
-#def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-#    print("Received message from topic '{}': {}".format(topic, payload))
-#    global received_count
-#    received_count += 1
-#    if received_count == cmdUtils.get_command("count"):
-#        received_all_event.set()
-#
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
+
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+
+# Callback when the subscribed topic receives a message
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("Received message from topic '{}': {}".format(topic, payload))
+    global received_count
+    received_count += 1
+    if received_count == cmdUtils.get_command("count"):
+        received_all_event.set()
+
+print("Script invoked with change {}".format(__name__))
 #print("Script invoked {}".format(__name__))
 #if __name__ == '__main__':
 #    mqtt_connection = cmdUtils.build_mqtt_connection(on_connection_interrupted, on_connection_resumed)
