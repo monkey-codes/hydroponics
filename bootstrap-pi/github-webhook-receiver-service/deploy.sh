0baf1756f08cfad0720ad1ec749ba06be05612b5
monkey codes <jz@monkey.codes>
2022-12-07 16:24:03 +1000

    Ensure sequential deploy

--- bootstrap-pi/github-webhook-receiver-service/deploy.sh
+++ bootstrap-pi/github-webhook-receiver-service/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-pull -U https://github.com/monkey-codes/hydroponics.git -i bootstrap-pi/hosts bootstrap-pi/local.yml
+flock --verbose /home/hydro/build.lock ansible-pull -U https://github.com/monkey-codes/hydroponics.git -i bootstrap-pi/hosts bootstrap-pi/local.yml
