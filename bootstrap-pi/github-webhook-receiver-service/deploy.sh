278e371b65ebe986ad404d324356ff696c5101d8
monkey codes <jz@monkey.codes>
2022-12-07 15:55:33 +1000

    Capture output

--- bootstrap-pi/github-webhook-receiver-service/deploy.sh
+++ bootstrap-pi/github-webhook-receiver-service/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-pull -U https://github.com/monkey-codes/hydroponics.git -i bootstrap-pi/hosts bootstrap-pi/local.yml > /tmp/last-deploy
+ansible-pull -U https://github.com/monkey-codes/hydroponics.git -i bootstrap-pi/hosts bootstrap-pi/local.yml 2>&1 | tee /tmp/last-deploy
