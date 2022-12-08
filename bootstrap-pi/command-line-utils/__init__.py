a6bc8e3ce8f6ccfffa042ff23659672d37c3205a
monkey codes <jz@monkey.codes>
2022-12-08 11:42:14 +1000

    Setup command line utils package

---
+++ bootstrap-pi/command-line-utils/__init__.py
@@ -0,0 +1,1 @@
+name = 'command_line_utils'
---
+++ bootstrap-pi/command-line-utils/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name="command_line_utils",
+    version="1.0.0",
+    author="demo_author",
+    author_email="demo@email.com",
+    description="AWS IOT command line utils",
+    long_description="AWS IOT command line utils",
+    long_description_content_type="text/markdown",
+    url="https://github.com/username/demopackage",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
