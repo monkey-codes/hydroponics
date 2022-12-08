713460997829423859246c13f87118d6dd63dd8b
monkey codes <jz@monkey.codes>
2022-12-08 12:50:50 +1000

    Remove duplicate log message

--- bootstrap-pi/upload-service/listener.py
+++ bootstrap-pi/upload-service/listener.py
@@ -70,10 +70,9 @@ def listen_for_upload_requests():
       server.listen(1)
       conn, addr = server.accept()
       datagram = conn.recv(4096)
-      print(datagram, flush=True)
       if datagram:
         tokens = datagram.strip().split()
-        print(datagram)
+        print(datagram, flush=True)
       conn.close()
 
 if __name__ == '__main__':
