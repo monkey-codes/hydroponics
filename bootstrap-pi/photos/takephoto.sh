0a284d4f6da06f45ddf1fb022d6cb9a4862286e1
monkey codes <jz@monkey.codes>
2023-01-31 08:54:19 +1000

    Flip image because camera is mounted upside down

--- bootstrap-pi/photos/takephoto.sh
+++ bootstrap-pi/photos/takephoto.sh
@@ -9,7 +9,7 @@ for CAMERA in /dev/video*; do
 	FILE_NAME=$(date +"%Y-%m-%dT%H:%M:%S%z").jpg
 	FILE_DIR=`pwd`/$CAMERA_ID
 	FILE_PATH=$FILE_DIR/$FILE_NAME
-	fswebcam -r 1920x1080 -d $CAMERA --skip 20 --set "Gamma"=100 --set "Hue"=0 --set "Sharpness"=2 --set "Saturation"=64 --set "White Balance Temperature"=6500 --set "Focus, Auto"=False --set "Focus (absolute)"=0 --set "Brightness"=-60 $FILE_PATH
+	fswebcam -r 1920x1080 -d $CAMERA --flip v --skip 20 --set "Gamma"=100 --set "Hue"=0 --set "Sharpness"=2 --set "Saturation"=64 --set "White Balance Temperature"=6500 --set "Focus, Auto"=False --set "Focus (absolute)"=0 --set "Brightness"=-60 $FILE_PATH
 	#fswebcam -r 1920x1080 -d $CAMERA --skip 20 --set "Focus, Auto"=False --set "Focus (absolute)"=10 --set "Brightness"=0 $FILE_PATH
 
 	find $FILE_DIR -name "*.jpg" -type f -size $MIN_FILE_SIZE -delete
